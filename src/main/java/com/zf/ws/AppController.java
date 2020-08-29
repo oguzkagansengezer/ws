ed151e3e3c7cae1c5414a53311bd2dc0d0248407
oguzkagansengezer <oguzkagansengezer@gmail.com>
2020-08-30 00:47:22 +0300

    make it better

--- src/main/java/com/zf/ws/AppController.java
+++ src/main/java/com/zf/ws/AppController.java
@@ -1,13 +1,13 @@
 package com.zf.ws;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String welcome() { 
-		return "test.jsp";
+		return "index";
 	}
 }
