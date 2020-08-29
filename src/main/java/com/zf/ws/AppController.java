3a62cb79897db86da86f0255d782961bbd8d0068
oguzkagansengezer <oguzkagansengezer@gmail.com>
2020-08-29 16:59:51 +0300

    first commit

---
+++ src/main/java/com/zf/ws/AppController.java
@@ -0,0 +1,13 @@
+package com.zf.ws;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String welcome() { 
+		return "test.jsp";
+	}
+}
